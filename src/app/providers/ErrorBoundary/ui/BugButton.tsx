e00aa53cd9abd3f129ec2b658c2bf391b7da91a7
krasterisk <info@krasterisk.ru>
2023-02-13 15:45:15 +0700

    Added Error Button translation

--- src/app/providers/ErrorBoundary/ui/BugButton.tsx
+++ src/app/providers/ErrorBoundary/ui/BugButton.tsx
@@ -1,10 +1,11 @@
-import { classNames } from 'shared/lib/classNames'
 import { useEffect, useState } from 'react'
 import { Button } from 'shared/ui/Button/Button'
+import { useTranslation } from 'react-i18next'
 
 // Компонент для тестирования ErrorBoundary
 export const BugButton = () => {
     const [error, setError] = useState(false)
+    const { t } = useTranslation()
 
     const OnThrow = () => { setError(true) }
 
@@ -18,7 +19,7 @@ export const BugButton = () => {
         <Button
             onClick={OnThrow}
         >
-            Throw Error
+            {t('Ошибка')}
         </Button>
     )
 }
