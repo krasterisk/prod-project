a97eb34a054074cdf9f5d204e8d8f08a173c9041
krasterisk <info@krasterisk.ru>
2023-02-17 16:08:31 +0700

    Fix unit tests

---
+++ src/shared/lib/tests/componentRender/componentRender.tsx
@@ -0,0 +1,22 @@
+import { type ReactNode } from 'react'
+import { render } from '@testing-library/react'
+import { I18nextProvider } from 'react-i18next'
+import i18nTest from 'shared/config/i18n/i18nTest'
+import { MemoryRouter } from 'react-router-dom'
+
+export interface ComponentRenderOptions {
+    route?: string
+}
+
+export function componentRender (component: ReactNode, options: ComponentRenderOptions = {}) {
+    const {
+        route = '/'
+    } = options
+    return render(
+        <MemoryRouter initialEntries={[route]}>
+            <I18nextProvider i18n={i18nTest}>
+                {component}
+            </I18nextProvider>
+        </MemoryRouter>
+    )
+}
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,20 +1,15 @@
 import React from 'react'
 import { classNames } from 'shared/lib/classNames/classNames'
 import cls from './Navbar.module.scss'
-import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink'
-import { useTranslation } from 'react-i18next'
 
 interface NavbarProps {
     className?: string
 }
 
 export const Navbar = ({ className }: NavbarProps) => {
-    const { t } = useTranslation()
-
     return (
         <div className={classNames(cls.Navbar, {}, [className])}>
             <div className={cls.links}>
-                /
             </div>
 
         </div>
