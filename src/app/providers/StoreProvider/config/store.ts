8fd96a4a55c5edf052c8b9942e81368c8dd7daed
krasterisk <info@krasterisk.ru>
2023-02-28 17:50:05 +0700

    Added async reducer manager, async login form {lint fix}

--- src/app/providers/StoreProvider/config/store.ts
+++ src/app/providers/StoreProvider/config/store.ts
@@ -1,4 +1,4 @@
-import { configureStore, DeepPartial, ReducersMapObject } from '@reduxjs/toolkit'
+import { configureStore, ReducersMapObject } from '@reduxjs/toolkit'
 import { StateSchema } from './StateSchema'
 import { counterReducer } from 'entities/Counter'
 import { userReducer } from 'entities/User'
