02e8e8b5489faa3f3471ff1b0f2e19923ea198cf
krasterisk <info@krasterisk.ru>
2023-03-12 18:45:01 +0700

    fix lint and ui tests

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,4 +1,4 @@
-import React, { memo, Suspense, useCallback, useMemo } from 'react'
+import React, { memo, Suspense, useCallback } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { AppRoutesProps, routeConfig } from 'shared/config/routeConfig/routeConfig'
 import { PageLoader } from 'widgets/PageLoader/PageLoader'
