c315acba05d71f68606496c799cb3e009a091fae
krasterisk <info@krasterisk.ru>
2023-04-19 09:21:05 +0700

    Added editableProfileCard test

--- src/features/EditableProfileCard/ui/EditableProfileCard/EditableProfileCard.tsx
+++ src/features/EditableProfileCard/ui/EditableProfileCard/EditableProfileCard.tsx
@@ -99,7 +99,7 @@ export const EditableProfileCard = memo((props: EditableProfileCardProps) => {
                         key={err}
                         theme={TextTheme.ERROR}
                         text={validateErrorTranslates[err]}
-                        data-testid="EditableProfileCard.error"
+                        data-testid="EditableProfileCard.Error"
                     />
                 ))}
                 <ProfileCard
