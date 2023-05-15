25474a3544266de6b561c6cd428915c0fcc0b476
krasterisk <info@krasterisk.ru>
2023-05-15 20:33:45 +0700

    fix storybook update

--- config/storybook/main.ts
+++ config/storybook/main.ts
@@ -1,5 +1,9 @@
 // import type { StorybookConfig } from '@storybook/react-vite'
 import type { StorybookConfig } from '@storybook/react-webpack5'
+import { buildPaths } from '../build/types/config'
+import path from 'path'
+import { DefinePlugin, RuleSetRule } from 'webpack'
+import { buildCssLoader } from '../build/loaders/buildCssLoader'
 const config: StorybookConfig = {
     stories: ['../../src/**/*.stories.@(js|jsx|ts|tsx)'],
     addons: [
@@ -15,6 +19,47 @@ const config: StorybookConfig = {
     //     name: '@storybook/react-vite',
     //     options: {}
     // },
+    webpackFinal: async (config, { configType }) => {
+        const paths: buildPaths = {
+            build: '',
+            html: '',
+            entry: '',
+            src: path.resolve(__dirname, '..', '..', 'src'),
+            locales: path.resolve(__dirname, '..', '..', 'public', 'locales'),
+            buildLocales: path.resolve(__dirname, '..', '..', 'build', 'locales')
+        }
+
+        config.resolve!.modules!.push(paths.src)
+        config.resolve!.extensions!.push('.ts', '.tsx')
+        config.resolve!.alias = {
+            ...config.resolve!.alias,
+            '@': paths.src
+        }
+
+        // @ts-expect-error
+        config.module!.rules = config.module!.rules!.map((rule: RuleSetRule) => {
+            if (rule?.test instanceof RegExp && rule.test.toString().includes('svg')) {
+                return { ...rule, exclude: /\.svg$/i }
+            }
+
+            return rule
+        })
+
+        config.module!.rules.push({
+            test: /\.svg$/,
+            use: ['@svgr/webpack']
+        })
+
+        config.module!.rules.push(buildCssLoader(true))
+
+        config.plugins!.push(new DefinePlugin({
+            __IS_DEV__: JSON.stringify(true),
+            __API__: JSON.stringify('https://localhost'),
+            __PROJECT__: JSON.stringify('storybook')
+        }))
+
+        return config
+    },
     docs: {
         autodocs: 'tag'
     }
